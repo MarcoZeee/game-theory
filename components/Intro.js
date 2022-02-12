e34dc547926b5232bcc1c3b0e0cc9a423f12ca4e
MarcoZeee <MarcoZeee@github.com>
2022-02-12 22:01:36 +0800

    delete console and text corrections

--- components/Intro.js
+++ components/Intro.js
@@ -1,10 +1,11 @@
 import styles from "../styles/Profile.module.css"
+import useSWR from "swr";
 
 const data = {
   approval: "Approval method basically counts the one candidate with the most votes, regradless of the ranking.",
-  current_system: "Refer to the definitions of current system FIFA and France Football in Glossary section",
+  current_system: "Reference to the definitions of current system FIFA and France Football in Glossary section",
   current_system_FIFA: "FIFA Best system 2021 takes 3 candidates each vote and score them 5, 3 and 1 points individually",
-  current_system_France_football: "Ballon D'or 2021 ranks 5 players from each counted vote 6, 4, 3, 2, 1 points, from most favoured to the least ",
+  current_system_France_football: "Ballon D'or 2021 ranks 5 players from each counted vote 6, 4, 3, 2, 1 points respectively, from most favoured to the least ",
   'popularity': "Popularity votes count one candidate with the most first choice selection",
   'runoff': "Instant runoff eliminates least popular candidate each round until there's only one candidate left",
   'borda_count': "Borda count scores n-th candidate with n - 1 points, and then sum them to reap the candidate with most points",
